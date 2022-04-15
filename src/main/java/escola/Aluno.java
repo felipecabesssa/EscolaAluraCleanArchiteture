be6015cd4d4dfd38f3c01eacb43c1b4e2af908e8
felipecabesssa <felipesoares100@hotmail.com>
2022-04-15 17:51:20 -0300

    Criando fabrica de alunos com metodos de instanciacao

--- src/main/java/escola/Aluno.java
+++ src/main/java/escola/Aluno.java
@@ -5,18 +5,24 @@ import java.util.List;
 
 public class Aluno {
 	
-	private String cpf;
+	private CPF cpf;
 	private String nome;
 	
 	private Email email;
 	
 	private List<Telefone> telefones = new ArrayList<>();
 	
+	public Aluno(CPF cpf, String nome, Email email) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.email = email;
+	}
+
 	public void adicionarTelefone(String ddd, String numero) {
 		this.telefones.add(new Telefone(ddd, numero));
 	}
 
-	public String getCpf() {
+	public CPF getCpf() {
 		return cpf;
 	}
 
